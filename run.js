c83f00d0220e401f6ffc8822af9361de34515f5d
stranothus <stranothus@gmail.com>
2022-04-13 11:10:32 -0500

    Make available as command

--- run.js
+++ run.js
@@ -1,6 +1,10 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 const { fromBrain } = require("./index.js");
 const file = process.argv[2];
+
+console.log(file);
 
 if(!file) throw new Error("Please specify a .bf file to run");
 
